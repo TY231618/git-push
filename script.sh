a0d5634d08668b021c4d2bda8589986569b1d905
TY231618 <ayoung@and.digital>
2023-03-29 14:44:10 +0100

    removing whitespace

--- script.sh
+++ script.sh
@@ -9,9 +9,9 @@ contents=$(jq --arg client "$1" --arg netmask "$2" '.instance_name_prefix=$clien
 
 echo "JQ DONE"
 
-ALL="$(base64 -i inventories/template/all.json)"
-CLIENT="$(base64 -i inventories/template/client.json)"
-HOST="$(base64 -i inventories/template/hosts)"
+ALL="$(base64 -i inventories/template/all.json | sed 's/ //g' )"
+CLIENT="$(base64 -i inventories/template/client.json | sed 's/ //g' )"
+HOST="$(base64 -i inventories/template/hosts | sed 's/ //g' )"
 
 echo "BASE64 DONE"
 
@@ -33,20 +33,20 @@ curl -L \
   https://api.github.com/repos/TY231618/git-push/contents/$1/all.yml \
   -d '{\"message\":\"my commit message\",\"committer\":{\"name\":\"Tony\",\"email\":\"ayoung@and.digital\"},\"content\":\"$ALL\"}'
 
-# curl -L \
-#   -X PUT \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: Bearer $3"\
-#   -H "X-GitHub-Api-Version: 2022-11-28" \
-#   https://api.github.com/repos/TY231618/git-push/contents/$1/client.yml \
-#   -d "{\"message\":\"my commit message\",\"committer\":{\"name\":\"Tony\",\"email\":\"ayoung@and.digital\"},\"content\":\"$CLIENT\"}"
+curl -L \
+  -X PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $3"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/TY231618/git-push/contents/$1/client.yml \
+  -d "{\"message\":\"my commit message\",\"committer\":{\"name\":\"Tony\",\"email\":\"ayoung@and.digital\"},\"content\":\"$CLIENT\"}"
 
-# curl -L \
-#   -X PUT \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: Bearer $3"\
-#   -H "X-GitHub-Api-Version: 2022-11-28" \
-#   https://api.github.com/repos/TY231618/git-push/contents/$1/hosts \
-#   -d "{\"message\":\"my commit message\",\"committer\":{\"name\":\"Tony\",\"email\":\"ayoung@and.digital\"},\"content\":\"$HOST\"}"
+curl -L \
+  -X PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $3"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/TY231618/git-push/contents/$1/hosts \
+  -d "{\"message\":\"my commit message\",\"committer\":{\"name\":\"Tony\",\"email\":\"ayoung@and.digital\"},\"content\":\"$HOST\"}"
 
 echo "CURL DONE"
