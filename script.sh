4ade35a23a73d5c28d309b3a2283d874111ae770
TY231618 <ayoung@and.digital>
2023-03-29 14:11:35 +0100

    update to script

--- script.sh
+++ script.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+echo "STARTING SCRIPT"
 contents=$(jq --arg client "$1" --arg netmask "$2" '.instance_name_prefix=$client | .netmask=$netmask' inventories/template/temp.json) && echo "${contents}" > inventories/template/all.json
+
+echo "JQ DONE"
 
 ALL="$(base64 -i inventories/template/all.json)"
 CLIENT="$(base64 -i inventories/template/client.json)"
 HOST="$(base64 -i inventories/template/hosts)"
+
+echo "BASE64 DONE"
 
 curl -L \
   -X PUT \
@@ -29,3 +33,5 @@ curl -L \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/TY231618/git-push/contents/$1/hosts \
   -d "{\"message\":\"my commit message\",\"committer\":{\"name\":\"Tony\",\"email\":\"ayoung@and.digital\"},\"content\":\"$HOST\"}"
+
+echo "CURL DONE"
