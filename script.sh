d86c674d4c5523564aa8d639517b4564f3272fef
TY231618 <ayoung@and.digital>
2023-03-29 14:18:15 +0100

    more echos to debug

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "STARTING SCRIPT"
+
+echo $1
+echo $2
+echo $3
+
 contents=$(jq --arg client "$1" --arg netmask "$2" '.instance_name_prefix=$client | .netmask=$netmask' inventories/template/temp.json) && echo "${contents}" > inventories/template/all.json
 
 echo "JQ DONE"
@@ -9,6 +14,16 @@ CLIENT="$(base64 -i inventories/template/client.json)"
 HOST="$(base64 -i inventories/template/hosts)"
 
 echo "BASE64 DONE"
+
+echo $ALL
+echo "SPACEMAN"
+
+echo $CLIENT
+echo "SPACEMAN"
+
+echo $HOST
+echo "SPACEMAN"
+
 
 curl -L \
   -X PUT \
